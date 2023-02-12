37ca1357d620bc7249e06bec2970222935d2d88d
hoangks5 <hoangnd.ute@gmail.com>
2023-02-12 15:11:57 +0700

    fix

--- auto.py
+++ auto.py
@@ -13,66 +13,89 @@ from telethon.tl.functions.photos import UploadProfilePhotoRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 import os
 from datetime import datetime
-
+from dotenv import load_dotenv, set_key
 api_id = 8759328
 api_hash = '1a270788cb618993f54f514f5a8c93c4'
 
 
 admin = ['hoangkss5']
-GROUP_ID = -868013328
+#GROUP_ID = -868013328
 
-def set_exchange_rate(number):
-     with open('exchange_rate.log','w') as f:
-          f.write(number)
-          f.close()
-def get_exchange_rate():
-    with open('exchange_rate.log','r') as f:
-        s = f.read()
-        f.close()
-    return int(s)
+GROUP_ID = 'hoangkss5'
 
-def set_total_value(number):
-     with open('total_value.log','w') as f:
-          f.write(number)
-          f.close()
-def get_total_value():
-    with open('total_value.log','r') as f:
-        s = f.read()
-        f.close()
-    return int(s)
+
+def set_up_config(ty_gia = None,ty_gia_vnd = None,ty_le = None):
+    # Tải tệp .env
+    load_dotenv()
+    # Chỉnh sửa giá trị của một khóa
+    if ty_gia != None:
+        set_key('.env', 'ty_gia', ty_gia)
+    if ty_le != None:
+        set_key('.env', 'ty_le', ty_le)
+    if ty_gia_vnd != None:
+        set_key('.env', 'ty_gia_vnd', ty_gia_vnd)
+def get_config(key):
+    load_dotenv()
+    # Lấy giá trị của một khóa
+    api_key = os.getenv(key)
+    return float(api_key)
+    
+
+
 
 client = TelegramClient('+84377820120', api_id, api_hash).start()
 @client.on(events.NewMessage())
 async def main(event):
         sender = await event.get_sender()
-        #print(event.message.raw_text)
         if sender.username in admin:
-            if '设置费率' in event.message.raw_text:
-                await client.send_message(GROUP_ID,'Đặt tỷ lệ '+''.join(re.findall(r'\d', event.message.raw_text))+'%')
-            if event.message.raw_text == '开始':
-                await client.send_message(GROUP_ID,'Bắt Đầu')
-            if '设置操作人' in event.message.raw_text:
+            if 'đặt tỷ lệ' in event.message.raw_text.lower():
+                number = ''.join(re.findall(r'\d+\.\d+', event.message.raw_text))
+                if number == '':
+                    number = ''.join(re.findall(r'\d', event.message.raw_text))
+                await client.send_message(GROUP_ID,'Đặt tỷ lệ '+number+'%')
+                set_up_config(ty_le=number)
+                
+                
+            if event.message.raw_text == 'Bắt Đầu':
+                await client.send_message(GROUP_ID,'Bắt Đầu Ghi Giao Dịch')
+                data = []
+            
+            if 'thêm quản lý' in event.message.raw_text.lower():
                 admin.append(event.message.raw_text.split('@')[1])
                 await client.send_message(GROUP_ID,'Thêm quản lý @'+event.message.raw_text.split('@')[1])
-            if '删除操作人' in event.message.raw_text:
+            if 'xóa quản lý' in event.message.raw_text.lower():
                 admin.append(event.message.raw_text.split('@')[1])
                 await client.send_message(GROUP_ID,'Xoá quản lý @'+event.message.raw_text.split('@')[1])
-            if '设置美元汇率' in event.message.raw_text:
-                exchange_rate = int(''.join(re.findall(r'\d', event.message.raw_text)))
-                await client.send_message(GROUP_ID,'Thiết lập tỷ giá bằng '+''.join(re.findall(r'\d', event.message.raw_text)))
-            if '设置越南盾汇率' in event.message.raw_text:
-                await client.send_message(GROUP_ID,'Thiết lập tỷ giá VND '+''.join(re.findall(r'\d', event.message.raw_text)))
-                set_exchange_rate(''.join(re.findall(r'\d', event.message.raw_text)))
+            
+            
+            if 'thiết lập tỷ giá bằng' in event.message.raw_text.lower():
+                number = ''.join(re.findall(r'\d+\.\d+', event.message.raw_text))
+                if number == '':
+                    number = ''.join(re.findall(r'\d', event.message.raw_text))
+                await client.send_message(GROUP_ID,'Thiết lập tỷ giá bằng '+number+'%')
+                set_up_config(ty_gia=number)
+                
+                
+            if 'thiết lập tỷ giá vnd' in event.message.raw_text.lower():
+                number = ''.join(re.findall(r'\d+\.\d+', event.message.raw_text))
+                if number == '':
+                    number = ''.join(re.findall(r'\d', event.message.raw_text))
+                await client.send_message(GROUP_ID,'Thiết lập tỷ giá VND '+number+'%')
+                set_up_config(ty_gia_vnd=number)
+                
+            
             if event.message.raw_text[0] == '+':
-
-                now = datetime.now()
-                current_time = now.strftime("%H:%M:%S")
-                code = ''.join(re.findall(r'\d', event.message.raw_text))
-                total_value = get_total_value()+int(code)
-                set_total_value(str(total_value))
-                result = int(code)*get_exchange_rate()
-                text = """Nhập mã(1 mã)\nThời gian: """+current_time+"""\n"""+code+""" * """+str(get_exchange_rate())+""" = """+str(result)+"""\n\nNhận tiền(0 mã)\n\n"""+"""Tổng nhập mã: """+str(total_value)+'\n'+'Phí đổi 0%\nVND: '+str(get_exchange_rate())+'\nCần trả: '+str(total_value*get_exchange_rate())+'\nCòn lại: '+str(total_value*get_exchange_rate())
-                await client.send_message(GROUP_ID,text)
+                try:
+                    now = datetime.now()
+                    current_time = now.strftime("%H:%M:%S")
+                    number = ''.join(re.findall(r'\d+\.\d+', event.message.raw_text))
+                    if number == '':
+                        number = ''.join(re.findall(r'\d', event.message.raw_text))
+                    data.append(current_time+'/'+number)
+                    await client.send_message(GROUP_ID,'Nhập mã\n'+'\n'.join(data))
+                except UnboundLocalError:
+                    await client.send_message(GROUP_ID,'Bạn chưa bắt đầu giao dịch\nĐể bắt đầu giao dịch hãy chat Bắt Đầu')
+                    
         else:
-              pass
+            pass
 client.run_until_disconnected()
